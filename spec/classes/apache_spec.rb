4ed0a9d2ccbe861e3fee80a58846517673e6c2c6
ottagit <ottaosawa@gmail.com>
2022-07-23 08:01:19 +0000

    ensure containment of module classes

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -6,7 +6,10 @@ describe 'apache' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
+      
+      it { is_expected.to contain_class('apache::install') }
+      it { is_expected.to contain_class('apache::config') }
+      it { is_expected.to contain_class('apache::service') }
       it { is_expected.to compile }
     end
   end
