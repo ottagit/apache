e368a980fcf59998a93d8732d2a3253cd35a02e0
ottagit <ottaosawa@gmail.com>
2022-08-02 04:47:13 +0000

    remove service name typo

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,7 +5,7 @@
 #   include apache::service
 class apache::service {
   service {
-    "${apache_service_name}":
+    "${apache::service_name}":
       alias      => 'apache_service',
       ensure     => $apache::service_ensure,
       enable     => $apache::service_enable,
